55846fa4464a96d996e1f9bfe5ad7d39aac306be
Alex.D <duanpengyuan@qq.com>
2014-03-24 22:46:47 +0800

    增加了tools包,编写了NetWorker抽象类,登陆逻辑大致完成

--- src/com/WindHunter/LoginActivity.java
+++ src/com/WindHunter/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.WindHunter;
 
+import android.widget.ProgressBar;
+import com.WindHunter.tools.*;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
@@ -14,6 +16,10 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        // 进度条不显示
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
         // 登录逻辑
         Button loginBtn = (Button)findViewById(R.id.submit);
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +31,7 @@ public class LoginActivity extends Activity {
                 String password = ((EditText) findViewById(R.id.password)).getText().toString();
 
                 // 检测空白输入
+                // TODO: 改为屏幕下面的浮动框，编辑框变红
                 if (email.isEmpty() || password.isEmpty()){
                     new AlertDialog.Builder(LoginActivity.this)
                             .setMessage(R.string.login_alert)
@@ -32,7 +39,44 @@ public class LoginActivity extends Activity {
                             .show();
                     return;
                 }
+
+
+                // TODO: 构造api
+                LoginNetWorker loginNetWorker = new LoginNetWorker();
+                loginNetWorker.execute("http://www.baidu.com");
             }
         });
+    }
+
+    private class LoginNetWorker extends NetWorker{
+
+        @Override
+        protected void onPostExecute(String result) {
+            findViewById(R.id.progressBar).setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+            findViewById(R.id.email).setEnabled(false);
+            findViewById(R.id.password).setEnabled(false);
+            findViewById(R.id.submit).setEnabled(false);
+        }
+
+        @Override
+        // TODO: 解析数据并放入全局共享对象中
+        protected void parser(String jsonStr) {
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage(jsonStr)
+                    .show();
+        }
+
+        @Override
+        // TODO: 需要改成浮动框
+        protected void alert() {
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage("NetWork Error")
+                    .show();
+        }
     }
 }
