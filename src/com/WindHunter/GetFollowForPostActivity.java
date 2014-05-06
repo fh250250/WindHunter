4988f0a4a6ad8baff157bb67c44e3c947b7440ea
Alex.D <duanpengyuan@qq.com>
2014-05-06 21:03:27 +0800

    修复@功能无关注的BUG

--- src/com/WindHunter/GetFollowForPostActivity.java
+++ src/com/WindHunter/GetFollowForPostActivity.java
@@ -224,8 +224,8 @@ public class GetFollowForPostActivity extends ActionBarActivity {
         });
 
 
-        // 组装关注用户API
-        String followApi = "http://" + host + "index.php?app=api&mod=User&act=user_follow";
+        // 组装 关注用户API
+        String followApi = "http://" + host + "index.php?app=api&mod=User&act=user_following";
         RequestParams requestParams = new RequestParams();
         requestParams.addQueryStringParameter("count", "100");
         requestParams.addQueryStringParameter("user_id", uid);
